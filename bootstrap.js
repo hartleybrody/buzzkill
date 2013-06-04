b5e377cc902403a6da40b7c725b0ad82081a5714
Hartley Brody <hartley.brody@gmail.com>
2013-06-04 16:37:11 -0400

    set the buzzkilling to on when the extension is installed

--- bootstrap.js
+++ bootstrap.js
@@ -1,4 +1,7 @@
-
+// when the extension is first installed
+chrome.runtime.onInstalled.addListener(function(details) {
+    localStorage["be_a_buzzkill"] = true;
+});
 // Listen for any changes to the URL of any tab.
 // see: http://developer.chrome.com/extensions/tabs.html#event-onUpdated
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
