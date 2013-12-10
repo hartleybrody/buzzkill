8340f3c72e50c319b7cbea858e6465b3936dd83c
Hartley Brody <hartley.brody@gmail.com>
2013-12-10 18:13:07 -0500

    major refactoring to clean up duplicate looping code

--- buzzkill.js
+++ buzzkill.js
@@ -4,54 +4,54 @@ function buzzkill(){
 
   // be a buzz kill in news feed & groups
   stories = document.getElementsByClassName("_5uch");
-
   for(var i=0; i < stories.length; i++){
     var story = stories[i];
+    killLinks(story, "feed");
 
-    // find any divs that contain the word "buzzfeed.com" (ie story captions)
-    var captions = story.getElementsByTagName("div");
-    for(var j=0; j < captions.length; j++){
-
-      if (captions[j].textContent.toLowerCase().indexOf("buzzfeed.com") !== -1 ){
-        killItem(story, "news feed url");
-      }
-    }
-
-    // look for any links to the buzzfeed fb page
-    var links = story.getElementsByTagName("a");
-    for(var k=0; k < links.length; k++){
-      var link = links[k];
-
-      if (actor.href.toLowerCase().indexOf("facebook.com/buzzfeed") !== -1 ){
-        killItem(story, "buzzfeed page");
-      }
-    }
   }
 
   // be a buzz kill on people's walls
   wall_posts = document.getElementsByClassName("fbTimelineUnit");
-
   for(var i=0; i < wall_posts.length; i++){
     var post = wall_posts[i];
-    var links = post.getElementsByTagName("a");
+    killLinks(post, "wall");
+  }
+}
 
-    // any wall posts that contain a buzzfeed url
-    for(var j=0; j < links.length; j++){
-      var href = links[j].href.toLowerCase();
-      if (href.indexOf("buzzfeed.com") !== -1 ||
-          href.indexOf("bzfd.it") !== -1 ||
-          href.indexOf("facebook.com/buzzfeed") !== -1){
-        killItem(post, "wall post");
-      }
+function killLinks(item, pageType){
+  var links = item.getElementsByTagName("a");
+  for(var k=0; k < links.length; k++){
+    var link = links[k];
+    var href = link.href.toLowerCase();
+    
+    // decide which type of link it is
+    var linkType = null;
+    if (href.indexOf("facebook.com/buzzfeed") !== -1 ){
+      linkType = "page link";
+    }
+    else if (href.indexOf("bzfd.it") !== -1 ){
+      linkType = "shortened link";
+    }
+    else if (href.indexOf("buzzfeed.com") !== -1 ){
+      linkType = "regular link";
+    }
+
+    // kill the story that contains this link
+    if(linkType !== null){
+      killItem(item, linkType, pageType);
     }
   }
 }
 
-function killItem(item, context){
-  item.style.opacity = "0.1";
+function killItem(item, linkType, pageType){
+
+  // set the story to be invisible
+  item.style.opacity = "0.0";
   item.style.display = "None";
+
+  // add this story to the list of killed stories
   if (killed_stories.indexOf(item) == -1){
-    console.log("got a " + context + " story");
+    console.log("killed a " + linkType + " on your " + pageType);
     killed_stories.push(item);
   }
 
