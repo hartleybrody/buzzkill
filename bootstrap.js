9bdadde53ff93fc10f27ccbd613294356a87527f
Hartley Brody <hartley.brody@gmail.com>
2015-01-27 15:28:06 -0500

    got it working w chrome storage

--- bootstrap.js
+++ bootstrap.js
@@ -1,11 +1,16 @@
 // when the extension is first installed
 chrome.runtime.onInstalled.addListener(function(details) {
-    localStorage["be_a_buzzkill"] = true;
+    chrome.storage.sync.set({be_a_buzzkill: true});
 });
 
 // Listen for any changes to the URL of any tab.
 // see: http://developer.chrome.com/extensions/tabs.html#event-onUpdated
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
+
+    // don't allow user to get to their page
+    if (tab.url.toLowerCase().indexOf("facebook.com/buzzfeed") !== -1){
+        chrome.tabs.update(tab.id, {url: "http://www.facebook.com/?no-buzzfeed-for-you!"});
+    }
 
     // decide if we're ready to show page action
     if (tab.status !== "complete" || tab.url.toLowerCase().indexOf("facebook.com") === -1){
@@ -14,14 +19,6 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
     }
     chrome.pageAction.show(tab.id);
 });
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.method == "get_status")
-      sendResponse({status: localStorage['be_a_buzzkill']});
-    else
-      sendResponse({}); // snub them.
-});
-
 
 
 // update the icon when the user's settings change
--- buzzkill.js
+++ buzzkill.js
@@ -3,43 +3,28 @@ var shouldKill;
 
 function buzzkill(){
 
-  console.log("GOT HERE");
-  console.log(shouldKill);
+  chrome.storage.sync.get("be_a_buzzkill", function(data){
+    if (data["be_a_buzzkill"]){
 
-  if (typeof shouldKill === undefined){
-    console.log('should we be killing?');
-    var self = this;
-    chrome.runtime.sendMessage({method: "get_status"}, function(response) {
-      self['shouldKill'] = response;
-      buzzkill(); // try again
-    });
-    return;
-  }
+      // be a buzz kill in news feed & groups
+      stories = document.getElementsByClassName("_5uch");
+      for(var i=0; i < stories.length; i++){
+        var story = stories[i];
+        killLinks(story, "feed");
 
-  else if (shouldKill === true){
+      }
 
-    // don't allow user to get to their page
-    if (tab.url.toLowerCase().indexOf("facebook.com/buzzfeed") !== -1){
-      chrome.tabs.update(tab.id, {url: "http://www.facebook.com/?no-buzzfeed-for-you!"});
+      // be a buzz kill on people's walls
+      wall_posts = document.getElementsByClassName("_5jmm");
+      for(var i=0; i < wall_posts.length; i++){
+        var post = wall_posts[i];
+        killLinks(post, "wall");
+      }
     }
-
-    // be a buzz kill in news feed & groups
-    stories = document.getElementsByClassName("_5uch");
-    for(var i=0; i < stories.length; i++){
-      var story = stories[i];
-      killLinks(story, "feed");
-
-    }
-
-    // be a buzz kill on people's walls
-    wall_posts = document.getElementsByClassName("fbTimelineUnit");
-    for(var i=0; i < wall_posts.length; i++){
-      var post = wall_posts[i];
-      killLinks(post, "wall");
-    }
-  }
-  
+  });
 }
+
+  
 
 function killLinks(item, pageType){
   var links = item.getElementsByTagName("a");
@@ -73,7 +58,7 @@ function killItem(item, linkType, pageType){
 
   // set the story to be invisible
   item.style.opacity = "0.5";
-  item.style.display = "None";
+  // item.style.display = "None";
 
   // add this story to the list of killed stories
   if (killed_stories.indexOf(item) == -1){
