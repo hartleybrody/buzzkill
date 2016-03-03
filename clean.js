085c7825f8bb0d40efd9bf19bf563bfc30b8ab6a
Hartley Brody <hartley_brody@intuit.com>
2016-03-03 16:55:20 -0500

    remember when I thought 2-spaces was cool for js... ugh

--- clean.js
+++ clean.js
@@ -11,71 +11,71 @@ var DEBUG_TERM = "trump"; // for testing
 
 function cleanNewsFeed(){
 
-  chrome.storage.sync.get("clean_news_feed", function(data){
-    if (data["clean_news_feed"]){
-      // find all potential posts
-      _.each(storyContainerClasses, function(storyContainerClass){
-        posts = document.getElementsByClassName(storyContainerClass);
-        _.each(posts, function(post){
-          removeLinks(post);
-          removeTerms(post);
-        });
-      });
-    }
-  });
+    chrome.storage.sync.get("clean_news_feed", function(data){
+        if (data["clean_news_feed"]){
+            // find all potential posts
+            _.each(storyContainerClasses, function(storyContainerClass){
+                posts = document.getElementsByClassName(storyContainerClass);
+                _.each(posts, function(post){
+                    removeLinks(post);
+                    removeTerms(post);
+                });
+            });
+        }
+    });
 }
 
 function removeLinks(item){
-  var links = item.getElementsByTagName("a");
-  _.each(links, function(link){
-    var href = link.href.toLowerCase();
-    _.each(bannedDomains, function(domain){
-      if (href.indexOf(domain) !== -1 || (DEBUG && href.indexOf(DEBUG_DOMAIN) !== -1)){
-        removeItem(item, "link", href);
-      }
+    var links = item.getElementsByTagName("a");
+    _.each(links, function(link){
+        var href = link.href.toLowerCase();
+        _.each(bannedDomains, function(domain){
+            if (href.indexOf(domain) !== -1 || (DEBUG && href.indexOf(DEBUG_DOMAIN) !== -1)){
+                removeItem(item, "link", href);
+            }
+        });
     });
-  });
 }
 
 function removeTerms(item){
-  var paragraphs = item.getElementsByTagName("p");
-  _.each(paragraphs, function(paragraph){
-    var text = paragraph.textContent.toLowerCase();
-    _.each(bannedTerms, function(term){
-      if (text.indexOf(term) !== -1 || (DEBUG && text.indexOf(DEBUG_TERM) !== -1)){
-        removeItem(item, "term in paragraph", term);
-      }
+    var paragraphs = item.getElementsByTagName("p");
+    _.each(paragraphs, function(paragraph){
+        var text = paragraph.textContent.toLowerCase();
+        _.each(bannedTerms, function(term){
+            if (text.indexOf(term) !== -1 || (DEBUG && text.indexOf(DEBUG_TERM) !== -1)){
+                removeItem(item, "term in paragraph", term);
+            }
+        });
     });
-  });
 
     var links = item.getElementsByTagName("a");
     _.each(links, function(link){
-      var text = link.textContent.toLowerCase();
-      _.each(bannedTerms, function(term){
-        if (text.indexOf(term) !== -1 || (DEBUG && text.indexOf(DEBUG_TERM) !== -1)){
-          removeItem(item, "term in link", term);
-        }
-      });
+        var text = link.textContent.toLowerCase();
+        _.each(bannedTerms, function(term){
+            if (text.indexOf(term) !== -1 || (DEBUG && text.indexOf(DEBUG_TERM) !== -1)){
+                removeItem(item, "term in link", term);
+            }
+        });
     });
 }
 
 function removeItem(item, offenseType, offenseMaterial){
 
-  // set the story to be invisible
-  if (DEBUG){
-    item.style.opacity = "0.5";
-  } else {
-    item.style.opacity = "0.0";
-    item.style.display = "None";
-  }
-
-  // add this story to the list of killed stories
-  if (removedStories.indexOf(item) == -1){
+    // set the story to be invisible
     if (DEBUG){
-      console.log("killed an item because of bad " + offenseType + ": " + offenseMaterial);
+        item.style.opacity = "0.5";
+    } else {
+        item.style.opacity = "0.0";
+        item.style.display = "None";
     }
-    removedStories.push(item);
-  }
+
+    // add this story to the list of killed stories
+    if (removedStories.indexOf(item) == -1){
+        if (DEBUG){
+            console.log("killed an item because of bad " + offenseType + ": " + offenseMaterial);
+        }
+        removedStories.push(item);
+    }
 
 }
 
